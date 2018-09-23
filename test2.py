619d30010b524a13964ec7c650f5bab5ff902f3a
GGMartin97 <42999995+GGMartin97@users.noreply.github.com>
2018-09-23 09:24:07 +0800

    test 2.0

---
+++ test2.py
@@ -0,0 +1,19 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+
+
+tilt=21
+
+GPIO.setup(tilt,GPIO.OUT)
+
+
+pwm=GPIO.PWM(tilt,50)
+
+pwm.start(0)
+
+pwm.ChangeDutyCycle(1)
+
+pwm.stop()
+
+GPIO.cleanup()
