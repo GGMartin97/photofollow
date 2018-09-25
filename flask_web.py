56c49b9c3c1850270ad2dfd8901f84c17f72ec5a
GGMartin97 <42999995+GGMartin97@users.noreply.github.com>
2018-09-25 15:58:53 +0800

    test

--- flask_web.py
+++ flask_web.py
@@ -18,32 +18,32 @@ def index():
 @app.route('/data1')
 def data1():
 	global move1
+	move1=move1+1
 	if move1<13 and move1>3:
-		move1=move1+1
 		hello.tmp1(move1)
 	return render_template('index.html')
 
 @app.route('/data2')
 def data2():
 	global move1
+	move1=move1-1
 	if move1<13 and move1>3:
-		move1=move1-1
 		hello.tmp1(move1)
 	return render_template('index.html')
 
 @app.route('/data3')
 def data3():
 	global move2
+	move2=move2+1
 	if move2<13 and move2>3:
-		move2=move2+1
 		hello.tmp2(move2)
 	return render_template('index.html')
 
 @app.route('/data4')
 def data4():
 	global move2
+	move2=move2-1
 	if move2<13 and move2>3:
-		move2=move2-1
 		hello.tmp2(move2)
 	return render_template('index.html')
 
